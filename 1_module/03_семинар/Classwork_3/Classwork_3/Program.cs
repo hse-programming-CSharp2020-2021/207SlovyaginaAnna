6e25cf18653cc08afac4e7d030dcfd91e2bc6c65
SlovyaginaAnna <70836672+SlovyaginaAnna@users.noreply.github.com>
2020-09-15 14:56:40 +0300

    Add files via upload

---
+++ 1_module/03_семинар/Classwork_3/Classwork_3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork_3
+{
+    class Program
+    {
+        public static void M1(int a)
+        {
+            switch (a)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("Неудовлетворительно");
+                    break;
+                case 4:
+                case 5:
+                    Console.WriteLine("удовлетворительно");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("Хорошо");
+                    break;
+                case 8:
+                case 9:
+                    Console.WriteLine("Отлично");
+                    break;
+                default:
+                    Console.WriteLine("Uncorrect");
+                    break;
+
+            }
+        }
+       
+        static void Main(string[] args)
+        {
+            string inp = Console.ReadLine();
+            int c;
+            if (int.TryParse(inp, out c)){
+                M1(c);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
---
+++ 1_module/03_семинар/Classwork_3/Z2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double old = 1, res = 0;
+            for(int i =1; old- res !=0; i++)
+            {
+                old = res;
+                res += 1/ ((double)i * (i + 1) * (i + 2));
+            }
+            Console.WriteLine(old);
+            Console.WriteLine(res);
+        }
+    }
+}
